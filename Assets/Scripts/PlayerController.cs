0a409c9ac85c820814501b6765cccbdd87ae8edc
atitap60-glitch <atit.ap60@gmail.com>
2026-03-27 16:54:10 +0700

    Lab11

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform FocalPoint;
     public float speed = 5f;
 
     private Rigidbody rb;
@@ -12,7 +14,9 @@ public class PlayerController : MonoBehaviour
     private InputAction smashAction;
     private InputAction breakAction;
 
+    public bool hasPowerUp = false;
 
+    private Coroutine countDownRoutine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -25,6 +29,43 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var  move = moveAction.ReadValue<Vector2>();
+        rb.AddForce(move.y * speed * FocalPoint.forward );
 
+        if (breakAction.IsPressed())
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("PowerUp"))
+        {   hasPowerUp = true;
+            Destroy(other.gameObject);
+            if (countDownRoutine != null)
+            {
+                StopCoroutine(countDownRoutine);
+            }
+            countDownRoutine = StartCoroutine(powerCoolDown());
+        }
+    }
+    IEnumerator powerCoolDown()
+    {
+        yield return new WaitForSeconds(10);
+        hasPowerUp = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Eenmy"))
+        {
+            if (!hasPowerUp == true)
+            {
+                var rb = collision.gameObject.GetComponent<Rigidbody>();
+                var dir = collision.transform.position - transform.position;
+                rb.AddForce(100 * dir.normalized, ForceMode.Impulse);
+            }
+        }
+    }
+    
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -7,7 +8,38 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
-
+        StartCoroutine(SpawnRoutine());
+        //InvokeRepeating(nameof(randomSpawn), 0, 5);
+        //StartCoroutine(Helllo());
+        //StartCoroutine(GoodBye());
     }
-
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            randomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+    }
+    void randomSpawn()
+    { 
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawn = spawnPoints[index];
+        Instantiate(enemyPrefab, spawn.position, Quaternion.identity);
+    }
+    IEnumerator GoodBye()
+    {
+        //yield return new WaitForSeconds(1);
+        Debug.Log("Bye " + Time.frameCount + "" + Time.time);
+        yield return null;
+        //yield break;
+        //StartCoroutine(Helllo());
+        //yield return Helllo();
+    }
+    IEnumerator Helllo()
+    {
+        Debug.Log("Hellow " + Time.frameCount);
+        yield return null;
+    }
 }
